4ed94e46de99e2a6dc0323f96c2c2f2b0e3a8f4b
CHANDREV <CHANDREV@CHANDREV03.corp.amdocs.com>
2017-04-20 17:37:03 +0530

    Longest Palindrome

---
+++ practice/src/main/java/com/chandresh/practice/programs/ds/string/LongestPalindrome.java
@@ -0,0 +1,47 @@
+package com.chandresh.practice.programs.ds.string;
+
+public class LongestPalindrome {
+
+	
+	public static void main(String args[]){
+		String inp = "gaoaoaoa";
+		
+		System.out.println(findLongestPalindrome(inp));
+	}
+
+	private static String findLongestPalindrome(String inp) {
+		if(inp==null)
+			return null;
+		
+		int start=-1,end=-1;
+		
+		for(int i=0;i<inp.length();i++){
+			int tempStart=-1,tempEnd=-1,j=i,k=inp.length()-1;
+			
+			while(j<=k){
+				if(j==k){
+					if(tempStart!=-1 && tempEnd!=-1 && (tempEnd-tempStart>end-start)){
+						start=tempStart;
+						end=tempEnd;
+					}
+					break;
+				}
+				if(inp.charAt(j)==inp.charAt(k)){
+					if(tempStart==-1)
+						tempStart=j;
+					if(tempEnd==-1)
+						tempEnd=k;
+					j++;k--;
+				}else{
+					tempStart=-1;
+					tempEnd=-1;
+					k--;
+				}
+			}
+		}
+		
+		if(start==-1 || end == -1)
+			return String.valueOf(inp.charAt(0));
+		return inp.substring(start, end+1);
+	}
+}
